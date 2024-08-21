39279e40b0796b3450893fdf641546478689633a
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2024-08-21 17:19:49 +0500

    update globals.css and Drawer.tsx : added cool neon effect to side bar

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -5,12 +5,12 @@ import { CgProfile } from "react-icons/cg";
 
 function SideNavbar() {
   return (
-    <div className=" ">
+    <div className="">
       <Disclosure as="nav">
         <DisclosureButton className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
         </DisclosureButton>
-        <div className="p-6 w-1/2 h-screen bg-[#c7fecf] bg-opacity-30 rounded-r-[20px]  backdrop-filter backdrop-blur-md fixed top-0 -left-96 lg:left-0 lg:w-60 peer-focus:left-0 peer:transition ease-out delay-150 duration-200 shadow-lg ">
-          <div className="flex flex-col justify-start items-center ">
+        <div className="p-6 w-1/2 h-screen bg-[#c7fecf] bg-opacity-30 backdrop-filter backdrop-blur-md fixed top-0 -left-96 lg:left-0 lg:w-60 peer-focus:left-0 peer:transition ease-out delay-150 duration-200 shadow-lg neon-border">
+          <div className="flex flex-col justify-start items-center">
             <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
               HealthBot
             </h1>

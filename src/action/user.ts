157ab0fd8239baa37001faa1fb3f09dbf3a08d1a
aalishan808 <aalishanhunzai@gmail.com>
2024-09-16 10:08:03 +0500

    password hashing done

--- src/action/user.ts
+++ src/action/user.ts
@@ -1,18 +1,19 @@
 "use server";
 
 import dbConnect from "@/lib/dbConnect";
-import { User } from "@/models/User";
-import { redirect } from "next/dist/client/components/redirect";
 
+import { redirect } from "next/dist/client/components/redirect";
+import {hash} from "bcryptjs";
+import User from "@/models/User";
 
 const register = async (formData: FormData) => {
 
 
-    const firstName = formData.get('firstname');
-    const lastName = formData.get('lastname');
-    const email = formData.get('email');
-    const password = formData.get('password');
-    const confirmPassword = formData.get('confirmpassword');
+    const firstName = formData.get('firstname') as string;
+    const lastName = formData.get('lastname') as string;
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmpassword') as string;
     console.log(firstName)
     console.log(lastName)
     console.log(email)
@@ -22,7 +23,9 @@ const register = async (formData: FormData) => {
     //searching for existing user
     const existingUser = await User.findOne({email})
     if(existingUser) throw new Error("User already Exists");
-    await User.create({firstname: firstName, lastname: lastName, email, password})
+
+    const hashedPassword = await hash(password, 12)
+    await User.create({firstname: firstName, lastname: lastName, email, password: hashedPassword})
     console.log("user created successfully");
     redirect('/login');
 };
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstname: { type: String },
-    lastname: { type: String },
-    email: { type: String },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, select: false },
-    role: { type: String, default: "Users" },
+    role: { type: String, default: "users" },
     image: { type: String },
     // Google provider
     authProviderId: { type: String },
@@ -14,4 +14,6 @@ const userSchema = new mongoose.Schema({
 console.log("Defining user model...");
 
 // Check if the model is already defined
-export const User = mongoose.models?.User || mongoose.model("Users", userSchema);
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;

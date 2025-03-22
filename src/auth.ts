123c3de316101f0665922c2d4cc875a53f7f8cc0
Shah Sarmad Hamdani <sarmadfarooqhamdani@gmail.com>
2025-03-23 01:09:26 +0500

    update 4 files and delete 3 files :: removes mongodb

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,5 @@
 import NextAuth, { CredentialsSignin } from "next-auth"
 import Credentials from "next-auth/providers/credentials";
-import dbConnect from "./lib/dbConnect";
-import User from "./models/User";
 import { compare } from "bcryptjs";
 import Google from "next-auth/providers/google";
 import { JWT } from "next-auth/jwt";
@@ -26,32 +24,30 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           throw new CredentialsSignin('Please provide both email and password')
         }
 
-        await dbConnect();
+        // TODO: Fix this with firebase
 
-        const user = await User.findOne({email}).select('+password');
-        console.log(user);
-        if(!user){
-          throw new CredentialsSignin('Invalid email or password')
+      //   if(!user){
+      //     throw new CredentialsSignin('Invalid email or password')
           
-        }
-        if(!user.password){
-          throw new Error("Invalid : password");
+      //   }
+      //   if(!user.password){
+      //     throw new Error("Invalid : password");
 
-        }
+      //   }
 
-        const isMatched = await compare(password,user.password)
-        if(!isMatched){
-          throw new Error("Invalid password");
-        }
-        const userData = {
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-          role:user.role,
-          id: user.id,
-        }
-        return userData;
-      }
+      //   const isMatched = await compare(password,user.password)
+      //   if(!isMatched){
+      //     throw new Error("Invalid password");
+      //   }
+      //   const userData = {
+      //     firstName: user.firstName,
+      //     lastName: user.lastName,
+      //     email: user.email,
+      //     role:user.role,
+      //     id: user.id,
+      //   }
+      //   return userData;
+      // }
     })
   ],
   pages:{
